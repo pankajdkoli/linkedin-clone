59472df7747dd16e1780c1cfd983f90396a01293
pankajdkoli <pankajdkoli@gmail.com>
2021-05-26 17:48:10 +0530

    import feed

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import './App.css';
+import Feed from './Feed';
 import Header from './Header.js';
 import Sidebar from './Sidebar.js';
 
 function App() {
   return (
     <div className="app">
-
+ 
       {/* Header */}
       <Header />
       {/* App body */}
       <div className="app_body">
         {/* sidebar */}
         <Sidebar />
+        <Feed />
         {/* feed */}
         {/* widgedts */}
       </div>
